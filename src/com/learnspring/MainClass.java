9f2064885ca0c0261d5f5f50cbda6a3641f61d1f
cpmangukiya <chintanpmangukiya@gmail.com>
2019-12-30 10:17:05 +0530

    interim commit

--- src/com/learnspring/MainClass.java
+++ src/com/learnspring/MainClass.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 public class MainClass {
     public static void main(String[] args) {
@@ -15,5 +18,21 @@ public class MainClass {
 
         Student studentConst = (Student) factory.getBean("studentBeanConst");
         studentConst.displayInfo();
+
+        ExpressionParser expressionParser = new SpelExpressionParser();
+        Expression hello_spEL = expressionParser.parseExpression("'Welcome SPEL'.concat('!')");
+        String message = (String) hello_spEL.getValue();
+        System.out.println("Hello SpEL output:"+message);
+
+        Expression exp = expressionParser.parseExpression("'Hello World'.bytes");
+        byte[] bytes = (byte[]) exp.getValue();
+        for(int i=0;i<bytes.length;i++){
+            System.out.print(bytes[i]+" ");
+        }
+        System.out.println();
+
+        Expression exp2 = expressionParser.parseExpression("new String('hello world').toUpperCase()");
+        String message2 = exp2.getValue(String.class);
+        System.out.println(message2);
     }
 }
